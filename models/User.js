952b10292b5ee9250d4c721580b13c5d245883fe
RoxasDavega <86384879+RoxasDavega@users.noreply.github.com>
2022-10-07 22:08:26 +0700

    Add files via upload

---
+++ models/User.js
@@ -0,0 +1,47 @@
+function createModeluser(Sequelize, DataTypes) {
+  const User = Sequelize.define("User", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    noHP: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+  });
+
+  return User;
+}
+
+module.exports = createModelUser;
